156338b64a80acad560af4c0bd886732eef37a14
Brandon Chen <brandonkachen@Brandons-MacBook-Pro.local>
2024-09-19 18:39:11 -0700

    fix: moved to common dir, not working

--- common/src/db/drizzle.config.ts
+++ common/src/db/drizzle.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'drizzle-kit'
 import { env } from './env.mjs'
+import path from 'path'
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './schema.ts',
+  schema: path.join(__dirname, 'schema.ts'),
   out: './migrations',
   dbCredentials: {
     url: env.DATABASE_URL,
