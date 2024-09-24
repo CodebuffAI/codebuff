63e95c6850eee694878398727b3553aceaa701d2
Brandon Chen <brandonkachen@Brandons-MacBook-Pro.local>
2024-09-24 14:15:02 -0700

    tweak: removed old comments

--- common/src/actions.ts
+++ common/src/actions.ts
@@ -58,46 +58,6 @@ export const ToolCallSchema = z.object({
 })
 export type ToolCall = z.infer<typeof ToolCallSchema>
 
-// export const CLIENT_ACTIONS = {
-//   userInput: z.object({
-//     type: z.literal('user-input'),
-//     fingerprintId: z.string(),
-//     userInputId: z.string(),
-//     messages: z.array(MessageSchema),
-//     fileContext: ProjectFileContextSchema,
-//     previousChanges: CHANGES,
-//   }),
-//   login: z.object({
-//     type: z.literal('login'),
-//     fingerprintId: z.string(),
-//   }),
-//   readFilesResponse: z.object({
-//     type: z.literal('read-files-response'),
-//     files: z.record(z.string(), z.union([z.string(), z.null()])),
-//   }),
-//   runTerminalCommand: z.object({
-//     type: z.literal('run-terminal-command'),
-//     command: z.string(),
-//   }),
-//   checkNpmVersion: z.object({
-//     type: z.literal('check-npm-version'),
-//     version: z.string(),
-//   }),
-//   warmContextCache: z.object({
-//     type: z.literal('warm-context-cache'),
-//     fingerprintId: z.string(),
-//     fileContext: ProjectFileContextSchema,
-//   }),
-// }
-
-// export const CLIENT_ACTION_SCHEMA = z.union([
-//   CLIENT_ACTIONS.userInput,
-//   CLIENT_ACTIONS.login,
-//   CLIENT_ACTIONS.readFilesResponse,
-//   CLIENT_ACTIONS.runTerminalCommand,
-//   CLIENT_ACTIONS.checkNpmVersion,
-//   CLIENT_ACTIONS.warmContextCache,
-// ])
 export const CLIENT_ACTION_SCHEMA = z.discriminatedUnion('type', [
   z.object({
     type: z.literal('user-input'),
@@ -134,53 +94,6 @@ export const CLIENT_ACTION_SCHEMA = z.discriminatedUnion('type', [
   }),
 ])
 export type ClientAction = z.infer<typeof CLIENT_ACTION_SCHEMA>
-
-// export const SERVER_ACTIONS = {
-//   responseChunk: z.object({
-//     type: z.literal('response-chunk'),
-//     userInputId: z.string(),
-//     chunk: z.string(),
-//   }),
-//   responseComplete: z.object({
-//     type: z.literal('response-complete'),
-//     userInputId: z.string(),
-//     response: z.string(),
-//     changes: CHANGES,
-//   }),
-//   readFiles: z.object({
-//     type: z.literal('read-files'),
-//     filePaths: z.array(z.string()),
-//   }),
-//   toolCall: z.object({
-//     type: z.literal('tool-call'),
-//     userInputId: z.string(),
-//     response: z.string(),
-//     data: ToolCallSchema,
-//     changes: CHANGES,
-//   }),
-//   terminalCommandResult: z.object({
-//     type: z.literal('terminal-command-result'),
-//     userInputId: z.string(),
-//     result: z.string(),
-//   }),
-//   npmVersionStatus: z.object({
-//     type: z.literal('npm-version-status'),
-//     isUpToDate: z.boolean(),
-//     latestVersion: z.string(),
-//   }),
-//   warmContextCacheResponse: z.object({
-//     type: z.literal('warm-context-cache-response'),
-//   }),
-// }
-// export const SERVER_ACTION_SCHEMA = z.union([
-//   SERVER_ACTIONS.responseChunk,
-//   SERVER_ACTIONS.responseComplete,
-//   SERVER_ACTIONS.readFiles,
-//   SERVER_ACTIONS.toolCall,
-//   SERVER_ACTIONS.terminalCommandResult,
-//   SERVER_ACTIONS.npmVersionStatus,
-//   SERVER_ACTIONS.warmContextCacheResponse,
-// ])
 
 export const SERVER_ACTION_SCHEMA = z.discriminatedUnion('type', [
   z.object({
