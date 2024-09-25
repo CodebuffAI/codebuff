421df7597286f685f88325984d549ee87916d5ba
Brandon Chen <brandonkachen@Brandons-MacBook-Pro.local>
2024-09-25 15:48:19 -0700

    fix: env vars for `npm-app`

--- npm-app/loadEnv.js
+++ npm-app/loadEnv.js
@@ -8,28 +8,29 @@ if (!process.env.ENVIRONMENT) {
   process.exit(1)
 }
 
-const DOTENV_PATH = process.env.ENVIRONMENT === 'local' ? '..' : '/etc/secrets'
-const path = `${DOTENV_PATH}/.env.${process.env.ENVIRONMENT}`
+const path = `../.env.${process.env.ENVIRONMENT}`
 console.log(`Using environment: ${process.env.ENVIRONMENT} (path: ${path})`)
 
 const envFileContent = fs.readFileSync(path, 'utf-8')
 const lines = envFileContent.split('\n')
-const env = {}
+const env = {
+  ENVIRONMENT: process.env.ENVIRONMENT,
+}
 
 lines.forEach((line) => {
   const trimmedLine = line.trim()
   if (!trimmedLine || trimmedLine.startsWith('#')) return
 
-  const [key, value] = trimmedLine.split('=')
+  const [key, v] = trimmedLine.split('=')
+  const value = v
+    .split("'")
+    .filter((t) => !!t)
+    .join('')
+    .trim()
 
-  match(key).with(
-    'ENVIRONMENT',
-    'APP_URL',
-    'NEXT_PUBLIC_BACKEND_URL',
-    (key) => {
-      env[key] = value
-    }
-  )
+  match(key).with('APP_URL', 'NEXT_PUBLIC_BACKEND_URL', (key) => {
+    env[key] = value
+  })
 })
 
 module.exports = Promise.resolve(env)
