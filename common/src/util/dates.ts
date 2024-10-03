a56a159a9d150806eec720936a0f25387f6a27f1
Brandon Chen <brandonkachen@Brandons-MacBook-Pro.local>
2024-10-03 12:52:33 -0700

    fix: subtle date infinite loop bug

--- common/src/util/dates.ts
+++ common/src/util/dates.ts
@@ -1,6 +1,6 @@
 export const getNextQuotaReset = (currentQuotaReset: Date | null): Date => {
   const now = new Date()
-  let nextMonth = currentQuotaReset ?? now
+  let nextMonth = new Date(currentQuotaReset ?? now)
   while (nextMonth <= now) {
     nextMonth.setMonth(nextMonth.getMonth() + 1)
   }
