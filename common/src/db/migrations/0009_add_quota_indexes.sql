265b11e6bd9074e5f0aa91415613660c874892d0
Brandon Chen <brandonchenjiacheng@gmail.com>
2024-11-24 22:13:38 -0800

    feat: added indexes for messages and fingerprints

--- common/src/db/migrations/0009_add_quota_indexes.sql
+++
@@ -1,5 +0,0 @@
--- Add index for the fingerprint_id foreign key in message table
-CREATE INDEX IF NOT EXISTS "message_fingerprint_id_idx" ON "message" ("fingerprint_id");
-
--- Add index for the user_id foreign key in message table  
-CREATE INDEX IF NOT EXISTS "message_user_id_idx" ON "message" ("user_id");
