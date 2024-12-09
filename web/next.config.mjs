851baffb326bf8b660a8185356e99e99df54c29a
Brandon Chen <brandonchenjiacheng@gmail.com>
2024-12-09 13:23:47 -0800

    fix: added back deleted next.config stuff

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -7,12 +7,41 @@ const withMDX = createMDX({
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
-  }
+  },
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  headers: () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ]
+  },
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'manicode.ai',
+          },
+        ],
+        permanent: false,
+        destination: `${env.NEXT_PUBLIC_APP_URL}/:path*`,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
