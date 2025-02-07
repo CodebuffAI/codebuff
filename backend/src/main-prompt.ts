c3282841491ec660208c6c30fbeef6a6e394e28b
Brandon Chen <brandonchenjiacheng@gmail.com>
2025-02-06 17:51:03 -0800

    fix: small tweaks to backend response for user

--- backend/src/main-prompt.ts
+++ backend/src/main-prompt.ts
@@ -332,7 +332,7 @@ export async function mainPrompt(
 
       onResponseChunk(`\nRelevant files:\n${existingFilePaths.join(' ')}\n`)
       fullResponse += `\nRelevant files:\n${existingFilePaths.join('\n')}\n`
-      onResponseChunk(`\nThinking deeply (can take a minute).\n\n`)
+      onResponseChunk(`\nThinking deeply (can take a minute!)`)
 
       logger.debug({ prompt, filePaths, existingFilePaths }, 'Thinking deeply')
       const planningStart = Date.now()
@@ -353,7 +353,7 @@ export async function mainPrompt(
         )
       fileProcessingPromises.push(...promises)
       // For now, don't print the plan to the user.
-      // onResponseChunk(`${plan}\n\n`)
+      // onResponseChunk(`${response}\n\n`)
       fullResponse += response + '\n\n'
       logger.debug(
         {
@@ -550,7 +550,7 @@ export async function mainPrompt(
   }
 
   if (fileProcessingPromises.length > 0) {
-    onResponseChunk('\nApplying file changes. Please wait.\n')
+    onResponseChunk('\nApplying file changes, please wait.\n')
   }
 
   const changes = (await Promise.all(fileProcessingPromises)).filter(
