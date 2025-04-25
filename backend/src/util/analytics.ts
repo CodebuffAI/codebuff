e90b19013d51845d0e7c9747c3402c8fd5b1f2bb
Venki <venkateshrameshkumar+1@gmail.com>
2025-04-24 17:07:02 -0700

    tweaks

--- backend/src/util/analytics.ts
+++ backend/src/util/analytics.ts
@@ -3,21 +3,20 @@ import { PostHog } from 'posthog-node'
 
 import { logger } from './logger'
 
+import { env } from '@/env.mjs'
+
 // Store the identified user ID
 let client: PostHog | undefined
 
 export function initAnalytics() {
-  if (
-    !process.env.NEXT_PUBLIC_POSTHOG_API_KEY ||
-    !process.env.NEXT_PUBLIC_POSTHOG_HOST_URL
-  ) {
+  if (!env.NEXT_PUBLIC_POSTHOG_API_KEY || !env.NEXT_PUBLIC_POSTHOG_HOST_URL) {
     throw new Error(
       'NEXT_PUBLIC_POSTHOG_API_KEY or NEXT_PUBLIC_POSTHOG_HOST_URL is not set'
     )
   }
 
-  client = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST_URL,
+  client = new PostHog(env.NEXT_PUBLIC_POSTHOG_API_KEY, {
+    host: env.NEXT_PUBLIC_POSTHOG_HOST_URL,
   })
 }
 export function flushAnalytics() {
@@ -36,7 +35,7 @@ export function trackEvent(
     throw new Error('Analytics client not initialized')
   }
 
-  if (process.env.NEXT_PUBLIC_CB_ENVIRONMENT !== 'production') {
+  if (env.NEXT_PUBLIC_CB_ENVIRONMENT !== 'production') {
     logger.info({ event }, 'Analytics event tracked')
     return
   }
