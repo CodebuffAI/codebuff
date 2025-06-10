a324a9ce3cc61a61fe6c09e09e1a668cbfe861dd
Brandon Chen <brandonchen@mac.local.meter>
2025-06-09 20:27:37 -0700

    next.config.mjs try 2

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,6 @@
 import { withContentlayer } from 'next-contentlayer'
 import createMDX from '@next/mdx'
-import { env } from '../env'
+import { env } from '../dist-env/env'
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
