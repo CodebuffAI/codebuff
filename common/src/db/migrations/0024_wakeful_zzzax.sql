1adc3ddb3a530af5ac890a290281544d4dabf105
James Grugett <jahooma@gmail.com>
2025-07-29 21:11:21 -0700

    Delete migrations

--- common/src/db/migrations/0024_wakeful_zzzax.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "publisher" DROP CONSTRAINT "publisher_slug_unique";--> statement-breakpoint
-ALTER TABLE "publisher" DROP COLUMN IF EXISTS "slug";--> statement-breakpoint
-ALTER TABLE "publisher" ALTER COLUMN "id" DROP DEFAULT;
