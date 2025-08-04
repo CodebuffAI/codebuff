cd0c7c1fcd153210e9a2d909110c16003448ac33
Charles Lien <charleslien97@gmail.com>
2025-08-04 15:25:07 -0700

    call codebuff binary

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -1,30 +1,77 @@
+import { execFileSync } from 'child_process'
+
 import type {
   CodebuffClientOptions,
-  Context,
+  ChatContext,
   ContinueChatOptions,
   NewChatOptions,
 } from './types'
 
+const CODEBUFF_BINARY = 'codebuff'
+
 export class CodebuffClient {
   private authToken: string
-  private abortController: AbortController
 
   public cwd: string
 
-  constructor({ apiKey, cwd, abortController }: CodebuffClientOptions) {
+  constructor({ apiKey, cwd }: CodebuffClientOptions) {
+    // TODO: download binary automatically
+    if (execFileSync('which', [CODEBUFF_BINARY]).toString().trim() === '') {
+      throw new Error(
+        'Codebuff binary not found. Please run "npm i -g codebuff"',
+      )
+    }
+
     this.authToken =
       apiKey.type === 'string'
         ? apiKey.value
         : process.env.CODEBUFF_API_KEY ?? ''
     this.cwd = cwd
-    this.abortController = abortController
   }
 
-  public newChat(options: NewChatOptions): Context {
-    return {}
+  public newChat({ agent, prompt, params }: NewChatOptions): ChatContext {
+    const args = [prompt, '-p', '--agent', agent]
+    if (params) {
+      args.push('--params', JSON.stringify(params))
+    }
+    if (this.cwd) {
+      args.push('--cwd', this.cwd)
+    }
+
+    const a = execFileSync(CODEBUFF_BINARY, args, {
+      stdio: 'pipe',
+      env: { CODEBUFF_API_KEY: this.authToken },
+    })
+
+    return {
+      agentId: agent,
+      chatId: '', // TODO
+    }
   }
 
-  public continueChat(options: ContinueChatOptions): Context {
-    return {}
+  public continueChat({
+    agent,
+    prompt,
+    params,
+    context,
+  }: ContinueChatOptions): ChatContext {
+    agent = agent ?? context.agentId
+    const args = [prompt, '-p', '--agent', agent]
+    if (params) {
+      args.push('--params', JSON.stringify(params))
+    }
+    if (this.cwd) {
+      args.push('--cwd', this.cwd)
+    }
+
+    const a = execFileSync(CODEBUFF_BINARY, args, {
+      stdio: 'pipe',
+      env: { CODEBUFF_API_KEY: this.authToken },
+    })
+
+    return {
+      agentId: agent,
+      chatId: '', // TODO
+    }
   }
 }
--- sdk/src/types.ts
+++ sdk/src/types.ts
@@ -1,11 +1,25 @@
+import type { AgentTemplateType } from '@codebuff/common/types/session-state'
+
 export type CodebuffClientOptions = {
   apiKey: { type: 'string'; value: string } | { type: 'env' }
   cwd: string
-  abortController: AbortController
 }
 
-export type Context = {}
+export type ChatContext = {
+  chatId: string
+  agentId: string
+}
 
-export type NewChatOptions = {}
+export type NewChatOptions = {
+  agent: AgentTemplateType
+  prompt: string
+  params?: Record<string, any>
+}
 
-export type ContinueChatOptions = {}
+export type ContinueChatOptions = {
+  context: ChatContext
+  agent?: AgentTemplateType
+  prompt: string
+  params?: Record<string, any>
+  chatId?: string
+}
