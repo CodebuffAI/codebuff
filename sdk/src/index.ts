2193955f89d3ecc82160d1379d54bb4e3c8daa6a
Daniel Hsu <dannyighsu@gmail.com>
2025-10-10 13:54:29 -0700

    Expose types in CodebuffClientOptions

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,15 +1,19 @@
-export type * from './run'
 export type * from '../../common/src/types/json'
 export type * from '../../common/src/types/messages/codebuff-message'
 export type * from '../../common/src/types/messages/data-content'
+export type * from '../../common/src/types/print-mode'
+export type * from './run'
 // Agent type exports
 export type { AgentDefinition } from '../../common/src/templates/initial-agents-dir/types/agent-definition'
 // Re-export code analysis functionality
 export * from '../../packages/code-map/src/index'
 
+export type { PublishedToolName } from '../../common/src/tools/constants'
+export type { ClientToolCall, ClientToolName, CodebuffToolOutput } from '../../common/src/tools/list'
 export * from './client'
 export * from './custom-tool'
-export * from './run-state'
-export * from './websocket-client'
 export * from './native/ripgrep'
+export * from './run-state'
 export { ToolHelpers } from './tools'
+export * from './websocket-client'
+
