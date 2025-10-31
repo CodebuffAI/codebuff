580783128ad33276ede1d9cea90028dabad64c95
brandonkachen <brandonchenjiacheng@gmail.com>
2025-10-30 17:52:07 -0700

    Update constants.ts

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -6,38 +6,7 @@ export const IS_DEV = env.NEXT_PUBLIC_CB_ENVIRONMENT === 'dev'
 export const IS_TEST = env.NEXT_PUBLIC_CB_ENVIRONMENT === 'test'
 export const IS_PROD = !IS_DEV && !IS_TEST
 
-const normalizeLocalWebPort = (inputEnv: NodeJS.ProcessEnv): string => {
-  const port = inputEnv.NEXT_PUBLIC_WEB_PORT
-  if (port && /^\d+$/.test(port)) {
-    return port
-  }
-  return String(env.NEXT_PUBLIC_WEB_PORT ?? 3000)
-}
-
-export const resolveWebsiteUrl = (
-  inputEnv: NodeJS.ProcessEnv = process.env,
-): string => {
-  const explicitUrl =
-    inputEnv.NEXT_PUBLIC_CODEBUFF_APP_URL || env.NEXT_PUBLIC_CODEBUFF_APP_URL
-  if (explicitUrl) {
-    return explicitUrl
-  }
-
-  const envName = inputEnv.NEXT_PUBLIC_CB_ENVIRONMENT
-  const isProdEnv =
-    envName === undefined
-      ? IS_PROD
-      : envName !== 'dev' && envName !== 'test'
-
-  if (isProdEnv) {
-    return 'https://codebuff.com'
-  }
-
-  const port = normalizeLocalWebPort(inputEnv)
-  return `http://localhost:${port}`
-}
-
-export const WEBSITE_URL = resolveWebsiteUrl()
+export const WEBSITE_URL = env.NEXT_PUBLIC_CODEBUFF_APP_URL
 
 const DEFAULT_BACKEND_URL = 'manicode-backend.onrender.com'
 const DEFAULT_BACKEND_URL_DEV = 'localhost:4242'
