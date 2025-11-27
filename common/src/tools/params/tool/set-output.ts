78c79e9980596b92883a20b4a68c4359be31a071
James Grugett <jahooma@gmail.com>
2025-11-27 12:37:25 -0800

    Give base2 the set_output tool so subagents can use it

--- common/src/tools/params/tool/set-output.ts
+++ common/src/tools/params/tool/set-output.ts
@@ -12,9 +12,9 @@ const inputSchema = z
     'JSON object to set as the agent output. This completely replaces any previous output. If the agent was spawned, this value will be passed back to its parent. If the agent has an outputSchema defined, the output will be validated against it.',
   )
 const description = `
-You must use this tool as it is the only way to report any findings to the user. Nothing else you write will be shown to the user.
+Subagents must use this tool as it is the only way to report any findings. Nothing else you write will be visible to the user/parent agent.
 
-Please set the output with all the information and analysis you want to pass on to the user. If you just want to send a simple message, use an object with the key "message" and value of the message you want to send.
+Please set the output with all the information and analysis you want to pass on. If you just want to send a simple message, use an object with the key "message" and value of the message you want to send.
 Example:
 ${$getNativeToolCallExampleString({
   toolName,
