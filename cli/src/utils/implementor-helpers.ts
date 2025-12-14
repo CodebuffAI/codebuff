bfa00020c90f88db4d94162622ef42a063683024
James Grugett <jahooma@gmail.com>
2025-12-13 16:05:52 -0800

    Show gpt 5.2

--- cli/src/utils/implementor-helpers.ts
+++ cli/src/utils/implementor-helpers.ts
@@ -44,7 +44,7 @@ export const getImplementorDisplayName = (
   } else if (agentType.includes('editor-implementor-gemini')) {
     baseName = 'Gemini'
   } else if (agentType.includes('editor-implementor-gpt-5')) {
-    baseName = 'GPT-5'
+    baseName = 'GPT-5.2'
   } else if (agentType.includes('editor-implementor')) {
     baseName = 'Sonnet'
   }
