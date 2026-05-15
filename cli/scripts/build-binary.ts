d780a9352b9d84f2bb673e594ee2e21c316c52f1
James Grugett <jahooma@gmail.com>
2026-05-15 14:34:51 -0700

    Use baseline Bun target for Windows builds

--- cli/scripts/build-binary.ts
+++ cli/scripts/build-binary.ts
@@ -95,7 +95,7 @@ function getTargetInfo(): TargetInfo {
       arch: 'arm64',
     },
     'win32-x64': {
-      bunTarget: 'bun-windows-x64',
+      bunTarget: 'bun-windows-x64-baseline',
       platform: 'win32',
       arch: 'x64',
     },
